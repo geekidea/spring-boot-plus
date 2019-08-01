70f38f7907a3495104d4be69dc78ed6501e0078b
geekidea <geekidea@qq.com>
2019-08-01 12:00:00 +0800

    fix swagger不能访问问题
    Former-commit-id: dcb8cdc9ef589dd7bac86fa830c26bf3de5fbfc4

--- src/main/java/io/geekidea/springbootplus/config/WebMvcConfig.java
+++ src/main/java/io/geekidea/springbootplus/config/WebMvcConfig.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
@@ -93,4 +94,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
