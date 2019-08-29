8e63cd15b03da6a3ecc159bac8f2a0afeb0994b2
springboot-plus <springbootplus@aliyun.com>
2019-08-29 12:13:10 +0800

    :computer: centos 快速安装mysql
    Former-commit-id: 31dcbc5088f7c6870852f17affb35aa6d7d37c06

--- docs/bin/install/install-mysql.sh
+++ docs/bin/install/install-mysql.sh
@@ -1,5 +1,29 @@
 #! /bin/shell
 
+# Copyright 2019-2029 geekidea(https://github.com/geekidea)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#======================================================================
+# 快速安装MySQL
+# CentOS7 中已成功验证
+# 使用yum+rpm方式安装
+#
+# author: geekidea
+# date: 2019-8-29
+#======================================================================
+
+
 # 配置阿里云yum镜像源
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
