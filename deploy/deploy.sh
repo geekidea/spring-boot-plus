88d01cfcdaef45ae29d4c58837e513e257a85b52
springboot-plus <springbootplus@aliyun.com>
2019-09-07 01:22:33 +0800

    :computer: 1.2.3.RELEASE CentOS快速安装项目运行环境，快速构建/运维部署/启动项目

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -23,44 +23,33 @@
 # date: 2019-9-6
 #======================================================================
 
-# 版本库路径
-PROJECT_REPO="https://github.com/geekidea/spring-boot-plus.git"
-# 项目名称
-PROJECT_NAME="spring-boot-plus"
-# 当前时间
-NOW=$(date --date='0 days ago' "+%Y-%m-%d-%H-%M-%S")
-echo "${NOW}"
-
 # 1. 下载或更新spring-boot-plus版本库
 # 先判断当前目录下是否有spring-boot-plus目录
 # 如果有，则执行git pull
 # 如果没有，则clone
-if [ ! -d "/spring-boot-plus" ]; then
-  git clone ${PROJECT_REPO}
+if [ ! -d "spring-boot-plus" ]; then
+  git clone https://github.com/geekidea/spring-boot-plus.git
 else
+  cd spring-boot-plus
   git pull
 fi
 
 # 2. maven打包
-cd $(PROJECT_NAME)
 mvn clean package -Ptest
 
 # 3. 停服
-# cd到项目源代码同级目录
-cd ../
-# 停服
-sh ${PROJECT_NAME}-server/bin/shutdown.sh
+cd ..
 
-# 4. 移动spring-boot-plus-server-assembly.tar.gz到项目同级目录下
-# 备份上一次的server
-echo "${NOW}"
-mv ${PROJECT_NAME}-server ${PROJECT_NAME}-server-"${NOW}"
-# 移动最新打包的server到项目同级目录下
-mv ${PROJECT_NAME}/target/${PROJECT_NAME}-server-assembly.tar.gz ../../
+if [ -d "spring-boot-plus-server" ]; then
+  sh spring-boot-plus-server/bin/shutdown.sh
+fi
+
+# 4. 复制spring-boot-plus-server-assembly.tar.gz到项目同级目录下
+cp spring-boot-plus/target/spring-boot-plus-server-assembly.tar.gz spring-boot-plus-server-assembly.tar.gz
 
 # 5. 运行spring-boot-plus
-tar -zxvf ${PROJECT_NAME}-server-assembly.tar.gz
-cd ${PROJECT_NAME}-server/bin
+tar -zxvf spring-boot-plus-server-assembly.tar.gz
+cd spring-boot-plus-server/bin
 sh restart.sh
 
 # 6. 访问项目
--- docs/bin/install/download-install-all.sh
+++ docs/bin/install/download-install-all.sh
@@ -22,12 +22,14 @@
 # date: 2019-8-29
 #======================================================================
 
-
+# 下载脚本
 wget -O install-all.sh https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/bin/install/install-all.sh
 wget -O install-jdk.sh  https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/bin/install/install-jdk.sh
 wget -O install-git.sh  https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/bin/install/install-git.sh
 wget -O install-maven.sh  https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/bin/install/install-maven.sh
 wget -O install-redis.sh  https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/bin/install/install-redis.sh
 wget -O install-mysql.sh  https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/bin/install/install-mysql.sh
+wget -O mysql_spring_boot_plus.sql https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/db/mysql_spring_boot_plus.sql
 
+# 执行安装所有
 sh install-all.sh
