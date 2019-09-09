f1ad03b14c2e5c2b5add955df14f7d37efc1e6b3
springboot-plus <springbootplus@aliyun.com>
2019-09-10 01:25:37 +0800

    :computer: 1.2.3.RELEASE maven aliyun mirror

--- docs/bin/install/install-maven.sh
+++ docs/bin/install/install-maven.sh
@@ -37,10 +37,20 @@ if [ $? != 1 ]
 then
     echo "Not Found maven"
     echo "Installing maven..."
+
+    # 下载 maven aliyun mirror settings.xml
+    wget -O settings.xml https://raw.githubusercontent.com/geekidea/spring-boot-plus/master/docs/config/settings.xml
+
     yum install -y maven
+
     hasMaven
-    if [ $? != 1 ]
+    if [ $? == 1 ]
     then
+      echo "Config Aliyun Maven Mirror..."
+      rm -rf /etc/maven/settings.xml
+      cp settings.xml /etc/maven/
+      mvn -version
+    else
       echo "Install maven Fail"
     fi
 fi
