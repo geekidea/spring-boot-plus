6fd16bc10a87a5f287ca333369c1713ce8876763
springboot-plus <springbootplus@aliyun.com>
2019-09-29 20:53:58 +0800

    springbootplus自定义异常

--- src/main/java/io/geekidea/springbootplus/common/exception/SpringBootPlusException.java
+++ src/main/java/io/geekidea/springbootplus/common/exception/SpringBootPlusException.java
@@ -1,12 +1,9 @@
-/**
+/*
  * Copyright 2019-2029 geekidea(https://github.com/geekidea)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,18 +13,26 @@
 
 package io.geekidea.springbootplus.common.exception;
 
+import lombok.Data;
+
 /**
- * 业务异常
+ * 自定义异常
  * @author geekidea
  * @date 2018-11-08
  */
-public class BusinessException extends SpringBootPlusException{
+@Data
+public class SpringBootPlusException extends RuntimeException{
+    private Integer errorCode;
+    private String message;
 
-    public BusinessException(String message) {
+    public SpringBootPlusException(String message) {
         super(message);
-    }
-    public BusinessException(Integer errorCode, String message) {
-        super(errorCode,message);
+        this.message = message;
     }
 
+    public SpringBootPlusException(Integer errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.message = message;
+    }
 }
