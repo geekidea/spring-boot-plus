9027046e321e5c8a011ef1896d21d1d214511c91
springboot-plus <springbootplus@aliyun.com>
2019-10-03 11:18:54 +0800

    :cn: 1.3.0.RELEASE shiro+jwt

--- src/main/java/io/geekidea/springbootplus/shiro/service/LoginService.java
+++ src/main/java/io/geekidea/springbootplus/shiro/service/LoginService.java
@@ -42,7 +42,12 @@ public interface LoginService {
     ApiResult login(LoginParam loginParam, HttpServletResponse response);
 
     /**
-     * 刷新token
+     * 如果(当前时间+倒计时) > 过期时间，则刷新token
+     * 并更新缓存
+     * 当前token失效，返回新token
+     * 当前请求有效，返回状态码：460
+     * 前端下次使用新token
+     * 如果token继续发往后台，则提示，此token已失效，请使用新token，不在返回新token，返回状态码：461
      *
      * @param jwtToken
      * @param httpServletResponse
