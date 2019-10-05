c0937c5b67d7624bb8136938e4c37275274a9bf7
springboot-plus <springbootplus@aliyun.com>
2019-10-05 18:09:23 +0800

    :cn: 1.3.0.RELEASE shiro+jwt

--- docs/db/mysql_spring_boot_plus.sql
+++ docs/db/mysql_spring_boot_plus.sql
@@ -59,20 +59,28 @@ INSERT INTO `sys_log` VALUES (1060438809495224322, 0, 'D', 100000, '2018-11-08 1
 -- Table structure for sys_user
 -- ----------------------------
 drop table if exists `sys_user`;
-create table sys_user(
-     id          bigint                              not null comment 'id',
-     name        varchar(20)                         null comment 'name',
-     account     varchar(20)                         not null comment 'account',
-     pwd         varchar(20)                         not null comment 'password',
-     remark      varchar(200)                        null comment 'remark',
-     create_time timestamp default CURRENT_TIMESTAMP null comment 'create time',
-     update_time timestamp                           null comment 'update time',
-     primary key (`id`),
-     constraint sys_user_account_uindex
-         unique (account)
-) comment 'SystemUser';
+create table sys_user
+(
+    id          bigint                              not null comment '主键'
+        primary key,
+    username    varchar(20)                         not null comment '用户名',
+    nickname    varchar(20)                         null comment '昵称',
+    password    varchar(64)                         not null comment '密码',
+    salt        varchar(32)                         null comment '盐值',
+    remark      varchar(200)                        null comment 'remark',
+    status      int       default 1                 not null comment '状态，0：禁用，1：启用',
+    create_time timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time timestamp                           null comment '修改时间',
+    constraint sys_user_username_uindex
+        unique (username)
+)
+    comment 'SystemUser';
+
+
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO sys_user (id, name, account, pwd, remark, create_time, update_time) VALUES (1, 'Administrator', 'admin', '123456', 'Administrator Account', '2019-08-26 00:52:01', null);
-
+INSERT INTO spring_boot_plus.sys_user (id, username, nickname, password, salt, remark, status, create_time, update_time)
+    VALUES (1, 'admin', '管理员', '751ade2f90ceb660cb2460f12cc6fe08268e628e4607bdb88a00605b3d66973c', 'e4cc3292e3ebc483998adb2c0e4e640e', 'Administrator Account', 1, '2019-08-26 00:52:01', null);
+INSERT INTO spring_boot_plus.sys_user (id, username, nickname, password, salt, remark, status, create_time, update_time)
+    VALUES (2, 'test', '测试人员', '751ade2f90ceb660cb2460f12cc6fe08268e628e4607bdb88a00605b3d66973c', '99952b31c18156169a26bec80fd211f6', 'Tester Account', 1, '2019-10-05 14:04:27', null);
