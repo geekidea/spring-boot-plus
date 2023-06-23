4a4607c21fc3dfd4cbec8dc2243b594226d056ae
geekidea <geekidea@qq.com>
2023-06-23 09:55:09 +0800

    操作日志列表筛选条件

--- src/main/java/io/geekidea/boot/system/query/SysLogQuery.java
+++ src/main/java/io/geekidea/boot/system/query/SysLogQuery.java
@@ -4,6 +4,8 @@ import io.geekidea.boot.framework.page.BasePageQuery;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 系统日志查询参数
  *
@@ -15,6 +17,29 @@ import lombok.Data;
 public class SysLogQuery extends BasePageQuery {
     private static final long serialVersionUID = 1L;
 
+    @Schema(description = "模块名称")
+    private String moduleName;
+
+    @Schema(description = "请求路径")
+    private String requestUrl;
+
+    @Schema(description = "日志名称")
+    private String logName;
+
+    @Schema(description = "操作人")
+    private String username;
+
+    @Schema(description = "日志类型 0:其它,1:新增,2:修改,3:删除,4:详情查询,5:所有列表,6:分页列表,7:其它查询,8:上传文件")
+    private Integer logType;
+
+    @Schema(description = "响应状态 false:失败,true:成功")
+    private Boolean responseSuccess;
+
+    @Schema(description = "创建开始时间")
+    private Date createStartTime;
+
+    @Schema(description = "创建结束时间")
+    private Date createEndTime;
 
 }
 
